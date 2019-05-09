efa3d327079e251e4c2c2504f83b25d75233a663
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-09 13:39:44 -0700

    almost done w/ post

--- friends/src/components/Form.jsx
+++ friends/src/components/Form.jsx
@@ -13,17 +13,33 @@ class FormSection extends Component {
       }
     }
   }
+
+  changeHandler = event => {
+    let value = event.target.value;
+    this.setState(prevState => ({
+      friend: {
+        ...prevState.friend,
+        [event.target.name]: value
+      }
+    }));
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.postFriend(this.state.friend)
+  }
+
   render() {
     return (
       <>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <FormGroup>
             <Label for="name" />
-            <Input type="name" name="name" id="name" placeholder="name" />
+            <Input type="name" name="name" id="name" placeholder="name" onChange={this.changeHandler} />
             <Label for="age" />
-            <Input type="age" name="age" id="age" placeholder="age" />
+            <Input type="age" name="age" id="age" placeholder="age" onChange={this.changeHandler} />
             <Label for="email" />
-            <Input type="email" name="email" id="email" placeholder="email" />
+            <Input type="email" name="email" id="email" placeholder="email" onChange={this.changeHandler} />
           </FormGroup>
           <Button>+friend</Button>
         </Form>
--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 const Friend = props => {
   return (
     <>
       <Container>
-        <Row className="friend text-left">
+        <Row className="friend text-left p-1">
           <Col>{props.friend.name}</Col>
           <Col>{props.friend.age}</Col>
           <Col>{props.friend.email}</Col>
+          <Button size="sm" color="danger">x</Button>
         </Row>
       </Container>
     </>
